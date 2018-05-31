1b85ee101f65a8461905b2c073f7e16f2a1661f6
Will Dembinski <willdembinski@gmail.com>
2018-05-30 18:47:16 -0700

    Better tests

--- __tests__/unit-tests.jest.js
+++ __tests__/unit-tests.jest.js
@@ -2,26 +2,47 @@
 "use strict";
 const explodey = require("../index");
 
+const attrs = {
+  a: true,
+  b: true,
+  c: true
+};
+
 describe("Its property accessing should work as expected", () => {
-  test(`It allows for accessing existing properties`, async done => {
-    const foo = explodey({
-      a: true,
-      b: true,
-      c: true
-    });
+  test(`It allows for accessing existing properties`, () => {
+    const foo = explodey(attrs);
     expect(foo.a).toBe(true);
     expect(foo.b).toBe(true);
     expect(foo.c).toBe(true);
   });
-  test(`It does not allow for accessing non-existing properties`, async done => {
-    const accessSomeProperty = (someProp, someObj) => () => someObj.someProp;
-    const bar = explodey({
-      a: true,
-      b: true,
-      c: true
+  test(`It does not allow for accessing non-existing properties`, () => {
+    const bar = explodey(attrs);
+    expect(() => bar.x).toThrow();
+  });
+  test(`JSON.stringify works`, () => {
+    const baz = explodey(attrs);
+    expect(JSON.stringify(baz)).toBe('{"a":true,"b":true,"c":true}');
+  });
+  test(`Spread operators work`, () => {
+    const biz = explodey(attrs);
+    expect({ ...biz }).toEqual(attrs);
+  });
+  test(`Spread operators work`, () => {
+    const nameForThing = explodey(attrs);
+    const otherNameForThing = { ...nameForThing };
+
+    expect(otherNameForThing).toEqual(attrs);
+    expect(otherNameForThing.quux).toBe(undefined);
+  });
+  test(`Nesting explodeys works`, () => {
+    const yyy = explodey({
+      x: explodey(attrs),
+      z: explodey(attrs)
     });
-    expect(accessSomeProperty("a", bar)).toThrow();
-    expect(accessSomeProperty("b", bar)).toThrow();
-    expect(accessSomeProperty("c", bar)).toThrow();
+    const otherExplodey = { ...yyy };
+
+    expect(yyy.x.a).toEqual(true);
+    expect(otherExplodey.x.a).toEqual(true);
+    expect(() => otherExplodey.x.quux).toThrow();
   });
 });
